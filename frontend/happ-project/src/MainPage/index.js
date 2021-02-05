057e11dc3b5673346e2f9de8935f89943fea72ca
MoizAliPi <mapirani.96@gmail.com>
2021-02-04 20:33:14 -0500

    Buttons and structure layout done

--- frontend/happ-project/src/MainPage/index.js
+++ frontend/happ-project/src/MainPage/index.js
@@ -5,23 +5,87 @@ import Stencil from "../Stencil";
 import styles from "./styles.module.scss";
 
 const MainPage = (props) => {
+    const [showKidsInfo, setKidsInfoToggle] = useState(false);
+    const [showNotKidsInfo, setNotKidsInfoToggle] = useState(false);
+
+    function toggleKidsInfo(){
+        setKidsInfoToggle(!showKidsInfo);
+    }
+
+    function toggleNotKidsInfo(){
+        setNotKidsInfoToggle(!showNotKidsInfo);
+    }
+
     return (
         <div className={styles.main}>
             <div className={styles.heading}><h3>Finding happiness...</h3></div>
             <h4 className={styles.infoHeader}>How to find it?</h4>
             <div className={styles.infoButtons}>
                 <div className={styles.kids}>
-                    <button>For Kids</button>
+                    <button onClick={toggleKidsInfo}>Tips For Kids</button>
                 </div>
                 <div className={styles.notKids}>
-                    <button>For not so Kids</button>
+                    <button onClick={toggleNotKidsInfo}>Tips for not so Kids</button>
                 </div>
             </div>
-            <div className={styles.kidsInfo}>
-                Here will be the information for kids....
+            <div className={`${styles.kidsInfo} ${showKidsInfo ? styles.show : styles.noShow}`}>
+                <div className={styles.infoBox}>
+                    <div className={styles.KidsPage}>
+                        <h3 className={styles.tipsHeading}>Okay, so what to do?</h3>
+                        <div className={styles.task}>
+                            <div className={styles.taskDetail}>1. Select the country from the list at the top corner.</div>
+                            <div><img className={styles.taskImg} src="" alt="select country image"/></div>
+                        </div>
+                        <div className={styles.task}>
+                            <div className={styles.taskDetail}>2. Click the shapes on the right and drag them to the similar holes.</div>
+                            <div><img className={styles.taskImg} src="" alt="Drag and drop image"/></div>
+                        </div>
+                        <div className={styles.task}>
+                            <div className={styles.taskDetail}>3. Make the shapes bigger or smaller so fit perfectly! That's it, eazy peezy!!</div>
+                            <div><img className={styles.taskImg} src="" alt="changing shape size image"/></div>
+                        </div>
+                        <div className={styles.task}>
+                            <div className={styles.taskDetail}>4. Point your mouse to each shape to know more about it!</div>
+                            <div><img className={styles.taskImg} src="" alt="pointing to the shape image"/></div>
+                        </div>
+                        <div className={styles.closeInfo}><button onClick={toggleKidsInfo}>Let's go!</button></div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.notKidsInfo}>
-                Here will be the information for adults
+            <div className={`${styles.notKidsInfo} ${showNotKidsInfo ? styles.show : styles.noShow}`}>
+                <div className={styles.infoBox}>
+                    <div className={styles.notKidsPage}>
+                        <h3 className={styles.tipsHeading}>So, what's this activity is about?</h3>
+                        <div className={styles.info}>
+                            <div className={styles.infoDetail}>
+                                Kids are supposed to select a country from the list and then drag them to its
+                                corresponding hole in the stencil below. What's interesting here is the hole 
+                                size is the size of the happiest country in the world Finland, so it basically 
+                                goes to shows that how a certain country is lacking/gaining in a category.
+                            </div>
+                            <div className={styles.infoImg}><img src="" alt="stencil1 image"/></div>
+                        </div>
+                        <div className={styles.info}>
+                            <div className={styles.infoDetail}>
+                                Each shape represents a category on which the happiness score is measured, Circle:
+                                Gdp Per Capita, Rectangle: Social Support, Triangle: Generosity, Square: Percept of
+                                corrupt, Pentagon: Freedom of choice, Star: Healthy life expectancy.
+                            </div>
+                            <div className={styles.infoImg}><img src="" alt="stencil2 image"/></div>
+                        </div>
+                        <div className={styles.info}>
+                            <div className={styles.infoDetail}>
+                                So when the children will select any shape and if it's smaller than the standard size
+                                that means that country needs to work on that category where as for any size bigger than
+                                standard will not require adjustment. Just to simply tell them what needs to be done in 
+                                order to make their country a little bit happier. Click <span><a href="#">here</a></span>
+                                to know more about it.
+                            </div>
+                            <div className={styles.infoImg}><img src="" alt="stencil3 image"/></div>
+                        </div>
+                        <div className={styles.closeInfo}><button onClick={toggleNotKidsInfo}>Activity time!</button></div>
+                    </div>
+                </div>
             </div>
             <Stencil />
             <div className={styles.aboutMe}>
--- frontend/happ-project/src/Stencil/index.js
+++ frontend/happ-project/src/Stencil/index.js
@@ -5,10 +5,38 @@ import Shape from "../Shape";
 import styles from "./styles.module.scss";
 
 const Stencil = (props) => {
+    const countries = ["Finland", "Denmark", "Switzerland",
+        "Iceland","Norway","Netherlands","Sweden","New Zealand","Austria","Luxembourg","Canada","Australia","United Kingdom","Israel","Costa Rica","Ireland","Germany","United States","Czech Republic","Belgium",
+        "United Arab Emirates","Malta","France","Mexico","Taiwan Province of China","Uruguay","Saudi Arabia","Spain","Guatemala","Italy","Singapore","Brazil","Slovenia","El Salvador","Kosovo","Panama","Slovakia","Uzbekistan","Chile","Bahrain","Lithuania",
+        "Trinidad and Tobago","Poland","Colombia","Cyprus","Nicaragua","Romania","Kuwait","Mauritius","Kazakhstan","Estonia","Philippines","Hungary","Thailand","Argentina","Honduras","Latvia","Ecuador","Portugal","Jamaica","South Korea", "Japan","Peru","Serbia",
+        "Bolivia","Pakistan","Paraguay","Dominican Republic","Bosnia and Herzegovina","Moldova","Tajikistan","Montenegro","Russia","Kyrgyzstan","Belarus","North Cyprus","Greece","Hong Kong S.A.R. of China","Croatia","Libya","Mongolia","Malaysia","Vietnam","Indonesia","Ivory Coast","Benin","Maldives","Congo (Brazzaville)",
+        "Azerbaijan","Macedonia","Ghana","Nepal","Turkey","China","Turkmenistan","Bulgaria","Morocco","Cameroon","Venezuela","Algeria","Senegal","Guinea","Niger","Laos","Albania",
+        "Cambodia","Bangladesh","Gabon","South Africa","Iraq","Lebanon","Burkina Faso","Gambia","Mali","Nigeria","Armenia","Georgia","Iran","Jordan","Mozambique","Kenya","Namibia","Ukraine","Liberia","Palestinian Territories","Uganda","Chad","Tunisia","Mauritania",
+        "Sri Lanka","Congo (Kinshasa)","Swaziland","Myanmar","Comoros","Togo","Ethiopia","Madagascar","Egypt","Sierra Leone","Burundi",
+        "Zambia","Haiti","Lesotho","India","Malawi","Yemen","Botswana","Tanzania","Central African Republic","Rwanda","Zimbabwe","South Sudan","Afghanistan"
+        ]
+
+    
     return (
         <div className={styles.stencilBox}>
-            This is the stencil container
-            <Shape />
+           <div className={styles.selectBox}>
+                <div className={styles.dropdown}>
+                    <div className={styles.customSelect}>
+                        <select>
+                        {countries.map((item, index) =>{
+                            return <option className={styles.customOption} name={index} value={item}>{item}</option>;
+                        })}
+                        </select>
+                    </div>
+                </div>
+                <div className={styles.draggableShapes}>
+                    Draggable Shapes
+                </div>
+           </div>
+           <div className={styles.dropBox}>
+                Stencils to place shapes
+                <Shape />
+           </div>
         </div>
     )
 }

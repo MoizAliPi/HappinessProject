42eb2730e6c3422310b03872a067dcc593c52861
MoizAliPi <mapirani.96@gmail.com>
2021-02-10 14:30:29 -0500

    Jupyter notebook updated

--- frontend/happ-project/src/Stencil/index.js
+++ frontend/happ-project/src/Stencil/index.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Shape from "../Shape";
 
+//import assets
+import { mdiChevronDown, mdiChevronUp } from "@mdi/js";
+import Icon from "@mdi/react";
+
 //import styles
 import styles from "./styles.module.scss";
 
@@ -15,18 +19,64 @@ const Stencil = (props) => {
         "Sri Lanka","Congo (Kinshasa)","Swaziland","Myanmar","Comoros","Togo","Ethiopia","Madagascar","Egypt","Sierra Leone","Burundi",
         "Zambia","Haiti","Lesotho","India","Malawi","Yemen","Botswana","Tanzania","Central African Republic","Rwanda","Zimbabwe","South Sudan","Afghanistan"
         ]
+    
+    const [country, setCountry] = useState('Select a country');
+    const [display, setListDisplay] = useState(false);
+    const [countryData, setCountryData] = useState('');
+
+    function toggleList(){
+        setListDisplay(!display);
+    }    
+
+    function changeCountry(e){
+        setCountry(e.target.innerHTML);
+        setListDisplay(!display);
+        console.log(e.target.innerHTML);
+    }
+
+    useEffect(() => {
+         // GET request using fetch inside useEffect React hook
+         fetch('https://happyie-project.herokuapp.com/country?name='+country)
+         .then(response => response.json())
+         .then(data => setCountryData(data))
+         .then(console.log(countryData));
+    
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    }, [country]);
+
+    function getData(){
+        // GET request using fetch with error handling
+        fetch('https://happyie-project.herokuapp.com/country?name='+country)
+        .then(async response => {
+            const data = await response.json();
+
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response statusText
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+            setCountryData(data)
+            console.log(countryData)
+        })
+        .catch(error => {
+            this.setState({ errorMessage: error.toString() });
+            console.error('There was an error!', error);
+        }); 
+    }
 
     
     return (
         <div className={styles.stencilBox}>
            <div className={styles.selectBox}>
-                <div className={styles.dropdown}>
-                    <div className={styles.customSelect}>
-                        <select>
-                        {countries.map((item, index) =>{
-                            return <option className={styles.customOption} name={index} value={item}>{item}</option>;
-                        })}
-                        </select>
+                <div className={styles.dropDown}>
+                    <button className={styles.dropDownBtn} onClick={toggleList}>{country} {display ? <Icon path={mdiChevronUp} size="35px" className={styles.selectIcon}/> : <Icon path={mdiChevronDown} size="35px" className={styles.selectIcon}/>}</button>
+                    <div className={styles.dropDownContent} style={{ display: display ? "flex" : "none" }}>
+                        <ul>
+                        {countries.map((item)=>
+                            <li className={styles.countryLabel} onClick={changeCountry}>{item}</li>
+                        )}
+                        </ul>
                     </div>
                 </div>
                 <div className={styles.draggableShapes}>
